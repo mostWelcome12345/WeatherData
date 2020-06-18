6368af6adbbe0e64e019f20d0e9248b82dfd656d
Onkar S <onkarsarkate22@gmail.com>
2020-06-18 18:22:27 +0530

    api code added

--- src/test/java/com/psl/AssignmentISV/WeatherDataApplicationTests.java
+++ src/test/java/com/psl/AssignmentISV/WeatherDataApplicationTests.java
@@ -1,13 +1,31 @@
 package com.psl.AssignmentISV;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.psl.AssignmentISV.Controller.WeatherControllerAPI;
 
 @SpringBootTest
 class WeatherDataApplicationTests {
+	@Autowired
+	private WeatherControllerAPI controller;
 
 	@Test
-	void contextLoads() {
-	}
+	public void contextLoads() throws JsonMappingException, JsonProcessingException {
+		ResponseEntity<String> s;
+		s = controller.getWeather("pune");
+		assertNotNull(s); // test case for checking result is not null
+		assertEquals(HttpStatus.OK , s.getStatusCode());
+		s = controller.getWeather("abc");
+		assertEquals(HttpStatus.BAD_REQUEST, s.getStatusCode());// test case for checking bad request
 
+	}
 }
